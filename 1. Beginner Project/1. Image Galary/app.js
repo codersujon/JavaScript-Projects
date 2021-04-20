967f5c733ba0692aa40fbf76545708a61336dd15
Md.Sujon Ahmed <shobuj2sujon@gmail.com>
2021-04-20 15:27:02 +0600

    Calculator Project HTML

--- 1. Beginner Project/1. Image Galary/app.js
+++ 1. Beginner Project/1. Image Galary/app.js
@@ -12,9 +12,31 @@ images.forEach(img => img.addEventListener('click', e => (current.src = e.target
 const current = document.querySelector('#current');
 const images = document.querySelectorAll('.images img');
 
+//Default Opacity
+const opacity = .4;
+//Set First Image Opacity
+images[0].style.opacity = opacity;
+
+
 images.forEach(function(img){ //ForEach Loop
 	img.addEventListener('click', function(e){
+		
+		//Reset Opacity to 1
+		images.forEach(img => (img.style.opacity = 1));
+		
+		//Current Image src change
 		current.src = e.target.src;
-	})
-});
+		
+		//Change Current Opacity
+		e.target.style.opacity = opacity;
+		
+		//Add Fade in Class
+		current.classList.add('fade-in');
+		//Remvoe Fade in Class
 
+		setTimeOut(function(){
+			current.classList.remove('fade-in');
+		}, 500);
+
+	});
+});
